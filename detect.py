a646cb038a875ff9cb67950aa78d222d4d4b9c86
moskovskayaliza2002 <moscovskayaliza@gmail.com>
2023-08-21 17:27:17 +0300

    made output in txt -> works faster

--- detect.py
+++ detect.py
@@ -18,16 +18,31 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
 from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
-
+def write_file_info(name, fps, weight, height, path):
+    with open(path, 'w') as f:
+        f.write(name + '\n')
+        f.write(str(fps) + '\n')
+        f.write(str(weight) + '\n')
+        f.write(str(height) + '\n')
+             
+             
+def write_txt(frame, bbox, bbox_scores, kps, path):
+    with open(path, 'a') as f:
+        b = ' '.join(str(i) for i in bbox)
+        k = ' '.join(str(i) for i in kps)
+        s = str(frame) + ' ' + b + ' ' + str(bbox_scores) + ' ' + k + '\n'
+        f.write(s)
+        
+        
 def detect(opt):
-    source, weights, view_img, save_txt, imgsz, save_txt_tidl, kpt_label, save_yaml = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, opt.save_txt_tidl, opt.kpt_label, opt.save_yaml
+    source, weights, view_img, save_txt, imgsz, save_txt_tidl, kpt_label = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, opt.save_txt_tidl, opt.kpt_label
     save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://', 'https://'))
 
     # Directories
     save_dir = increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok)  # increment run
-    (save_dir / 'labels' if (save_txt or save_txt_tidl or save_yaml) else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    (save_dir / 'labels' if (save_txt or save_txt_tidl) else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
     
     print(f"SAVE PATH {save_dir}")
     yml_data = []
@@ -64,23 +79,31 @@ def detect(opt):
         dataset = LoadStreams(source, img_size=imgsz, stride=stride)
     else:
         dataset = LoadImages(source, img_size=imgsz, stride=stride)
-
+        
+    if source[-3:] == 'MOV' or source[-3:] == 'mp4':
+        cap = cv2.VideoCapture(source)
+        filename = source[source.rfind('/'):source.rfind('.')]
+        yml_filename = '/home/ubuntu/yolo7/yolov7/' + str(save_dir) + '/labels' + filename
+        txt_path = yml_filename + '.txt'
+        FPS = cap.get(cv2.CAP_PROP_FPS)
+        weight = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        write_file_info(source, FPS, weight, height, txt_path)
+    
     # Run inference
     if device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
     t0 = time.time()
+    frame_count = 0
     for path, img, im0s, vid_cap in dataset:
+        tstart_i = time.time()
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
         
-        if save_yaml:
-            FPS = vid_cap.get(cv2.CAP_PROP_FPS)
-            weight = vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-            height = vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-            name = source
+        frame_count += 1
         # Inference
         t1 = time_synchronized()
         pred = model(img, augment=opt.augment)[0]
@@ -94,7 +117,6 @@ def detect(opt):
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            frame = 1
             if webcam:  # batch_size >= 1
                 p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
             else:
@@ -102,12 +124,9 @@ def detect(opt):
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # img.jpg
-            if dataset.mode == 'video':
-                filename = source[source.rfind('/'):source.rfind('.')]
-                yml_filename = '/home/ubuntu/yolo7/yolov7/' + str(save_dir) + '/labels' + filename
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            #txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            #gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 scale_coords(img.shape[2:], det[:, :4], im0.shape, kpt_label=False)
@@ -119,17 +138,23 @@ def detect(opt):
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Write results
-                if save_yaml:
-                    bboxes = []
-                    bboxes_scores = []
-                    pred_kp = []
-                    
                 for det_index, (*xyxy, conf, cls) in enumerate(reversed(det[:,:6])):
+                    '''
                     if save_txt:  # Write to file
+                        txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+                        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                    '''
+                    if save_txt:  # Write to file
+                        xywh = torch.tensor(xyxy).view(1, 4)
+                        bbox = [int(xywh[0,0]), int(xywh[0,1]), int(xywh[0,0] + xywh[0,2]), int(xywh[0,1] + xywh[0,3])]
+                        bs = conf.item()
+                        kpts = det[det_index, 6:].tolist()
+                        pred_kp = [int(kpts[0]), int(kpts[1]), int(kpts[3]), int(kpts[4])]
+                        write_txt(frame_count, bbox, bs, pred_kp, txt_path)
 
                     if save_img or opt.save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
@@ -138,18 +163,6 @@ def detect(opt):
                         plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=opt.line_thickness, kpt_label=kpt_label, kpts=kpts, steps=3, orig_shape=im0.shape[:2])
                         if opt.save_crop:
                             save_one_box(xyxy, im0s, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
-                    if save_yaml:
-                        #xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        xywh = torch.tensor(xyxy).view(1, 4)
-                        bboxes.append([int(xywh[0,0]), int(xywh[0,1]), int(xywh[0,0] + xywh[0,2]), int(xywh[0,1] + xywh[0,3])])
-                        bboxes_scores.append(conf.item())
-                        kpts = det[det_index, 6:].tolist()
-                        pred_kp.append([[int(kpts[0]), int(kpts[1])],[int(kpts[3]), int(kpts[4])]])
-                
-                if save_yaml:
-                    yml_data.append(OrderedDict({'frame': frame, 'bboxes': bboxes, 'bboxes_scores': bboxes_scores, 'keypoints': pred_kp}))
-                    frame += 1
-
 
                 if save_txt_tidl:  # Write to file in tidl dump format
                     for *xyxy, conf, cls in det_tidl:
@@ -157,14 +170,6 @@ def detect(opt):
                         line = (conf, cls,  *xyxy) if opt.save_conf else (cls, *xyxy)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
-            if save_yaml:
-                with open(yml_filename + '.yml', 'w') as f:
-                    yaml.add_representer(OrderedDict, lambda dumper, data: dumper.represent_mapping('tag:yaml.org,2002:map', data.items()))
-                    yaml.dump(OrderedDict({'name': name, 'FPS': FPS, 'weight': weight, 'height': height}), f)
-                with open(yml_filename + '.yml', 'a') as f:
-                    yaml.add_representer(OrderedDict, lambda dumper, data: dumper.represent_mapping('tag:yaml.org,2002:map', data.items()))
-                    data = OrderedDict({'frames': yml_data})
-                    yaml.dump(data, f)
                     
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')
@@ -192,6 +197,8 @@ def detect(opt):
                             save_path += '.mp4'
                         vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer.write(im0)
+                    
+        print(f'Frame done. ({time.time() - tstart_i:.3f}s)')
 
     if save_txt or save_txt_tidl or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt or save_txt_tidl else ''
@@ -226,7 +233,6 @@ if __name__ == '__main__':
     parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     parser.add_argument('--kpt-label', action='store_true', help='use keypoint labels')
-    parser.add_argument('--save-yaml', action='store_true', help='use keypoint labels')
     opt = parser.parse_args()
     print(opt)
     check_requirements(exclude=('tensorboard', 'pycocotools', 'thop'))
